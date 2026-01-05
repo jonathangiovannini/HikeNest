644878783403f959a0ae07f00b17fa618d2532f4
jonathangiovannini <jonathangiovannini17@gmail.com>
2026-01-05 11:01:14 +0100

    Errore deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/HikeNest/",
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false
+  },
 })
