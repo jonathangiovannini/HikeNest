d0bf9561f7e897dd8ad6629d9d91c0c451279cff
Jonathan Giovannini <jonathangiovannini@Mac.lan>
2026-02-16 16:51:06 +0100

    Aggiunti link base dei social

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ function Footer() {
 
                         <ul className="mt-8 flex justify-center gap-6 sm:mt-0 lg:justify-end">
                             <li>
-                                <a href="#" rel="noreferrer" target="_blank"
+                                <a href="https://www.facebook.com" rel="noreferrer" target="_blank"
                                     className="text-gray-700 transition hover:opacity-75 text-shadow-mine-shaft-700">
                                     <span className="sr-only">Facebook</span>
 
@@ -42,7 +42,7 @@ function Footer() {
                             </li>
 
                             <li>
-                                <a href="#" rel="noreferrer" target="_blank"
+                                <a href="https://www.instagram.com" rel="noreferrer" target="_blank"
                                     className="text-gray-700 transition hover:opacity-75 text-shadow-mine-shaft-700">
                                     <span className="sr-only">Instagram</span>
 
@@ -55,19 +55,18 @@ function Footer() {
                             </li>
 
                             <li>
-                                <a href="#" rel="noreferrer" target="_blank"
+                                <a href="https://www.x.com" rel="noreferrer" target="_blank"
                                     className="text-gray-700 transition hover:opacity-75 text-shadow-mine-shaft-700">
                                     <span className="sr-only">Twitter</span>
 
-                                    <svg className="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                        <path
-                                            d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+                                    <svg className="size-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
                                     </svg>
                                 </a>
                             </li>
 
                             <li>
-                                <a href="#" rel="noreferrer" target="_blank"
+                                <a href="https://www.github.com" rel="noreferrer" target="_blank"
                                     className="text-gray-700 transition hover:opacity-75 text-shadow-mine-shaft-700">
                                     <span className="sr-only">GitHub</span>
 
