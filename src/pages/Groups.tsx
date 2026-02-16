f61312696473a82c4bd469011ff2e156bce7c1b9
Jonathan Giovannini <jonathangiovannini@Mac.lan>
2026-02-16 17:06:39 +0100

    Altro errore di radius

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -220,7 +220,7 @@ function Groups() {
                                     <div className="p-6">
                                         <div className="flex justify-between items-start mb-4">
                                             <h3 className="text-xl font-bold text-mine-shaft-950 leading-tight">{g.nome}</h3>
-                                            <span className={`px-2 py-1 rounded text-[10px] font-black uppercase ${
+                                            <span className={`px-2 py-1 text-[10px] font-black uppercase ${
                                                 g.esperienza === 'esperto' ? 'bg-red-100 text-red-600' : g.esperienza === 'medio'? 'bg-blue-100 text-blue-600' :'bg-green-100 text-green-600'
                                             }`}>
                                                 {g.esperienza}
